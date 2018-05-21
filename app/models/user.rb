8ad34954f86e093f4c35ac4beeb341723a2169c2
Emily Boucher <emi.boucher@gmail.com>
2018-05-21 18:55:50 -0400

    upate model, add invitable, edit, change password pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  
-extend Devise::Models
-include Friendlyable
+  extend Devise::Models
+  include Friendlyable
+  belongs_to :account, optional: true
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resource :accounts
 
   get 'user/me', to: 'users#me', as: 'my_settings'
-  patch 'users/update_me', to: 'users# update_me', as: 'update_my_settings'
+  patch 'users/update_me', to: 'users#update_me', as: 'update_my_settings'
   get 'user/password', to: 'users#password', as: 'my_password'
   patch 'user/update_password', to:'users#update_password', as: 'my_update_password'
 
